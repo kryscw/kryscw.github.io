a22d0b75c620df2db986ff04ce014bf14d2c5e91
kryscw <ethandubau@gmail.com>
2025-03-21 00:29:26 -0500

    update v1.0.0 to the terminal

--- terminal.js
+++ terminal.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     scroll();
 
     var addText = function(textToAdd) {
+        clearInput();
         document.getElementById('terminalResultsCont').innerHTML += textToAdd + "</p>";
         scroll();
     };
@@ -22,6 +23,8 @@ document.addEventListener('DOMContentLoaded', function () {
             "help -> receive list of commands",
             "calltrain (south_sector, north_sector) -> call a train",
             "checkstatus (botname) -> check status of AFR-4790 robot",
+            "ping (servername) -> ping a server",
+            "request (info) -> pull from city personell database",
             "clear -> clears terminal"
         ].join("<br><br>");
         addText(helpList);
@@ -34,20 +37,44 @@ document.addEventListener('DOMContentLoaded', function () {
     var checkStatus = function (name) {
         switch (name) {
             case "echo":
-                clearInput();
                 addText("Rogue; Do not interact with this Bot.");
                 break;
             case "delta":
-                clearInput();
                 addText("Signs normal; kept under preservation in NORTH SECTOR.");
                 break;
             case "foxtrot":
-                clearInput();
                 addText("Ping was not received.");
                 break;
             default:
-                clearInput();
                 addText("Bot could not be found.");
+                break;
+        }
+    }
+
+    var reqInfo = function (info) {
+        if (info == "pengy") {
+                addText("Mayor of Bit City. Has faced plenty of backlash<br><br> from U.S. courts, but persists.");
+        } else if (info == "echo" || info == "foxtrot" || info == "delta") {
+                addText("AFR-4790 machine built with dubious morality to fulfill the purpose<br><br>of robotic takeover. Often prone to corruption.")
+        } else if (info == "rifle" || info == "boomgun" || info == "sten") {
+                addText("Weapons created for AFR-4790s to handle. Upon their deactivation,<br><br> their weapons were scattered among the city.");
+        } else {
+                addText("Usage: request (pengy, echo/foxtrot/delta, rifle/boomgun/sten, etc.)");
+        }
+    }
+
+    var pingServer = function (server) {
+        switch (server) {
+            case "wyoming":
+                addText("Pinging WYOMING with 32 bytes of data:");
+                addText("Reply from WYOMING: bytes=32 time=62ms TTL=10");
+                break;
+            case "bit city":
+                addText("Pinging BIT CITY with 32 bytes of data:");
+                addText("Reply from BIT CITY: bytes=32 time=2ms TTL=220");
+                break;
+            default:
+                addText("Usage: ping (wyoming, bit city, etc.)");
                 break;
         }
     }
@@ -56,18 +83,16 @@ document.addEventListener('DOMContentLoaded', function () {
         switch(txtLower){
             case "help":
                 postHelp();
-                clearInput();
                 break;
             case "calltrain south_sector":
-                clearInput();
                 addText("Calling train bound to SOUTH SECTOR...");
                 break;
             case "calltrain north_sector":
-                clearInput();
                 addText("Calling train bound to NORTH SECTOR...");
                 break;
+            case "pengy":
+                addText("")
             default:
-                clearInput();
                 addText("Bad command or filename")
                 break;
         }
@@ -82,8 +107,11 @@ document.addEventListener('DOMContentLoaded', function () {
             if (txtInput.includes("checkstatus")) {
                 checkStatus(txtInput.replace('checkstatus ', ''));
             } else if (txtInput.includes("clear")) {
-                clearInput();
                 clearTerminal();
+            } else if (txtInput.includes("request")) {
+                reqInfo(txtInput.replace('request ', ''));
+            } else if (txtInput.includes('ping')) {
+                pingServer(txtInput.replace('ping ', ''));
             } else {
                 checkCmds();
             }
